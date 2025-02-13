a560ade60002c1fd6269d1f068201e6c2105f8f5
Marco Oosthuizen <marco.oosthuizen@entelect.co.za>
2025-02-13 10:40:28 +0200

    init

--- lib/cdk_cicd_test-stack.ts
+++ lib/cdk_cicd_test-stack.ts
@@ -1,16 +1,42 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class CdkCicdTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC with default settings (public and private subnets)
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 1,
+      natGateways: 0,
+      subnetConfiguration: [{
+        cidrMask: 24,
+        name: 'PublicSubnet',
+        subnetType: ec2.SubnetType.PUBLIC
+      }]
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCicdTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define the security group
+    const securityGroup = new ec2.SecurityGroup(this, 'MySecurityGroup', {
+      vpc,
+      description: 'Allow SSH access',
+      allowAllOutbound: true
+    });
+
+    // Allow SSH access from anywhere (for testing purposes, restrict in production)
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH');
+
+    const instance = new ec2.Instance(this, 'Instance', {
+      vpc,
+      instanceType: new ec2.InstanceType('t3.micro'),
+      machineImage: ec2.MachineImage.genericLinux({
+        'af-south-1': 'ami-0085381100f11374c',
+      }),
+      securityGroup,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      associatePublicIpAddress: true,
+    });
+
   }
 }
